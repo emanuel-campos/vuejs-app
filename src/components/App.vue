8359111c429fba83da6d835ff1f12974451b4bc2
Emanuel Campos <emanuelcampos.contato@gmail.com>
2017-12-13 01:15:25 -0300

    initial styles to components

--- src/components/App.vue
+++ src/components/App.vue
@@ -15,6 +15,112 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+	$defaultFont: 400 14px Open Sans, Helvetica Neue,Helvetica,Arial,sans-serif;
 
+	body{
+		background: #ecf0f1;
+		padding: 0;
+		margin: 0;
+	}
+
+	/*secao geral para todas as paginas da aplicacao*/
+	.app{
+		color: #000;
+		font: $defaultFont;
+	}
+
+	/*caixas que separam o conteudo em secoes*/
+	.widget {
+		background-color: #fff;
+	    border-radius: 3px;
+	    margin: 20px auto;
+	    padding: 20px;
+
+	    .widget--title {
+		    border-bottom: 1px solid #ecf0f1;
+		    border-radius: 0;
+		    margin: -20px -20px 20px;
+		    overflow: hidden;
+		    padding: 10px 20px;
+
+		    .icon{
+		    	margin-right: 10px;
+		    }
+		}
+	}
+
+	/*estilo para os formularios*/
+	.form--input{
+		margin-top: 20px;
+	}
+
+	.form--input--field{
+		background-color: #ecf0f1;
+	    border: 1px solid rgba(0,0,0,.05);
+	    border-radius: 3px;
+	    display: inline-block;
+	    font: $defaultFont;
+	    font-size: 14px;
+	    padding: 10px;
+	    transition: opacity .3s;
+	    width: 100%;
+	    box-sizing: border-box;
+	}
+
+	.form--button{
+	    appearance: none;
+	    background-color: #2b84d2;
+	    border: 1px solid rgba(0,0,0,.05);
+	    border-radius: 3px;
+	    color: #fff;
+	    cursor: pointer;
+	    display: inline-block;
+	    font: $defaultFont;
+	    overflow: hidden;
+	    padding: 10px 20px;
+	    position: relative;
+	    text-align: center;
+	    transition: background-color .1s,opacity .1s;
+	    vertical-align: middle;
+
+	    /*classes para alinhamento dos botoes*/
+	    &.right{
+	    	float: right;
+	    }
+
+	    &.left{
+	    	float: left;
+	    }
+
+	    /*classes para os tipos de botoes*/
+	    &.text{
+    	    color: #563dd2;
+		    background-color: transparent;
+		    border-color: transparent;
+	    }
+
+	    &.paddingless{
+	    	padding-left: 0;
+	    	padding-right: 0;
+	    }
+	}
+
+	.form--buttons{
+		margin: 40px 0 0;
+		text-align: center;
+
+		&.left{
+			text-align: left;
+		}
+	}
+
+	/*estilo especifico para o form do tipo 'block'*/
+	.form--input--block{
+		.form--input--label{
+			display: block;
+		    margin: 5px 0;
+		    text-align: left;
+		}
+	}
 </style>
--- src/components/users/UserLogin.vue
+++ src/components/users/UserLogin.vue
@@ -2,9 +2,9 @@
 	<section class="app login">
 		<main class="app--main">
 			<section class="users--login">
-				<form class="users--form form---input--block widget">
+				<form class="users--form form--input--block widget">
 					<div class="widget--title">
-						App | Entrar
+						<i class="icon fa fa-unlock-alt"></i> App | Entrar
 					</div>
 					<div class="form--input">
 						<label for="email" class="form--input--label">E-mail</label>
@@ -13,14 +13,14 @@
 						</div>
 					</div>
 					<div class="form--input">
-						<label for="password" class="form--input--label">E-mail</label>
+						<label for="password" class="form--input--label">Senha</label>
 						<div class="form--input--wrapper">
 							<input id="password" class="form--input--field" type="password" placeholder="Digite sua senha aqui...">
 						</div>
 					</div>
-					<div class="form--buttons">
+					<div class="form--buttons left">
 						<button class="form--button">Fazer Login</button>
-						<a class="form--button right">Não é cadastrado?</a>
+						<a class="form--button right text paddingless">Não é cadastrado?</a>
 					</div>
 				</form>
 			</section>
@@ -38,6 +38,14 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+	.users--login {
+	    margin: 10px auto;
+	    max-width: 360px;
 
+	    .widget {
+		    border-radius: 3px;
+		    margin: 10px
+		}
+	}
 </style>
