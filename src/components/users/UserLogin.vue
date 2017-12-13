ff77e73875e68cae6b1d32dcb752ebee23b5b311
Emanuel Campos <emanuelcampos.contato@gmail.com>
2017-12-12 23:00:20 -0300

    create html structure of component UserLoginComponent

--- src/components/users/UserLogin.vue
+++ src/components/users/UserLogin.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>
-  	Login
-  </div>
+	<section class="app login">
+		<main class="app--main">
+			<section class="users--login">
+				<form class="users--form form---input--block widget">
+					<div class="widget--title">
+						App | Entrar
+					</div>
+					<div class="form--input">
+						<label for="email" class="form--input--label">E-mail</label>
+						<div class="form--input--wrapper">
+							<input id="email" class="form--input--field" type="email" placeholder="exemplo@email.com">
+						</div>
+					</div>
+					<div class="form--input">
+						<label for="password" class="form--input--label">E-mail</label>
+						<div class="form--input--wrapper">
+							<input id="password" class="form--input--field" type="password" placeholder="Digite sua senha aqui...">
+						</div>
+					</div>
+					<div class="form--buttons">
+						<button class="form--button">Fazer Login</button>
+						<a class="form--button right">Não é cadastrado?</a>
+					</div>
+				</form>
+			</section>
+		</main>
+	</section>
 </template>
 
 <script>
